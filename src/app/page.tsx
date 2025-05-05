a918291c7e659f9cb8d7366e0c0fbb13e497b7a0
BlackSquirrel <Maexxla3458@proton.me>
2025-05-05 12:46:09 +0200

    removed unused variables 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,16 @@ import Text from "@/components/Text";
 import Workshops from "@/components/Workshops";
 import Aktuell from "@/components/Aktuell";
 import Kontakt from "@/components/Kontakt";
-import { navLinks } from '@/app/lib/data';
-import { aktuellws } from '@/app/lib/data';
-
+import { navLinks } from "@/app/lib/data";
+// import { aktuellws } from '@/app/lib/data';
 
 export default function Home() {
   return (
-    <div className="bg-darkbg static items-center
-    font-[family-name:var(--font-geist-sans)] text-white">
-      <Navbar links={navLinks.home}/>
+    <div
+      className="bg-darkbg static items-center
+    font-[family-name:var(--font-geist-sans)] text-white"
+    >
+      <Navbar links={navLinks.home} />
       <Hero />
       <Text />
       <Aktuell />
